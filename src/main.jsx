8da97e836d549fdb360f5213917099101a1124ef
Sara <s.cristal.p@gmail.com>
2026-07-14 19:57:52 +0200

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
 import "./index.css";
 import App from "./App.jsx";
-import { AuthProvider } from "./auth/AuthContext";
+import { AuthProvider } from "./auth/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>,
 );
