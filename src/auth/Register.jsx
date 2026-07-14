5a80018b0a24f322a2116e789707955b49fcbde6
Sara <s.cristal.p@gmail.com>
2026-07-14 19:53:40 +0200

    update register.jsx

--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import { Link, useNavigate } from "react-router";
+
 import { useAuth } from "./AuthContext";
-import { usePage } from "../layout/PageContext";
 
 /** A form that allows users to register for a new account */
 export default function Register() {
   const { register } = useAuth();
-  const { setPage } = usePage();
+  const navigate = useNavigate();
 
   const [error, setError] = useState(null);
 
@@ -14,9 +15,10 @@ export default function Register() {
 
     const username = formData.get("username");
     const password = formData.get("password");
+
     try {
       await register({ username, password });
-      setPage("activities");
+      navigate("/activities");
     } catch (e) {
       setError(e.message);
     }
@@ -25,21 +27,24 @@ export default function Register() {
   return (
     <>
       <h1>Register for an account</h1>
+
       <form action={tryRegister}>
         <label>
           Username
           <input type="text" name="username" required />
         </label>
+
         <label>
           Password
           <input type="password" name="password" required />
         </label>
+
         <button>Register</button>
+
         {error && <p role="alert">{error}</p>}
       </form>
-      <a onClick={() => setPage("login")}>
-        Already have an account? Log in here.
-      </a>
+
+      <Link to="/login">Already have an account? Log in here.</Link>
     </>
   );
 }
