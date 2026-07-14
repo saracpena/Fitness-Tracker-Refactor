252fb6bf6200a9efb3a834411dd02b69da1b62ff
Sara <s.cristal.p@gmail.com>
2026-07-14 19:52:07 +0200

    update layout with comments

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,18 @@
+import { Outlet } from "react-router";
 import Navbar from "./Navbar";
 
 /** The shared layout for all pages of the app */
-export default function Layout({ children }) {
+export default function Layout() {
   return (
-    <>
+    <div>
       <Navbar />
-      <main>{children}</main>
-    </>
+      <main>
+        <Outlet />
+      </main>
+    </div>
   );
 }
+
+///activities → ActivitiesPage appears in Outlet
+//login      → Login appears in Outlet
+//register   → Register appears in Outlet
